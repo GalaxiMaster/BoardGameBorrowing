37d3b1b902378ea7410a7f2c0bd71677a75415a1
Dylan <dmj08bot@gmail.com>
2024-12-30 22:47:14 +1100

    simple selection of the hub, with back implimentation

--- lib/Pages/borrow_screen.dart
+++ lib/Pages/borrow_screen.dart
@@ -2,35 +2,41 @@ import 'package:board_game_borrower/Colors.dart';
 import 'package:board_game_borrower/widgets.dart';
 import 'package:flutter/material.dart';
 
-class BorrowScreen extends StatelessWidget{
+class BorrowScreen extends StatefulWidget {
   const BorrowScreen({super.key});
 
   @override
+  _BorrowScreenState createState() => _BorrowScreenState();
+}
+
+class _BorrowScreenState extends State<BorrowScreen> {
+  Map gameData = {
+    'name': 'Catan',
+    'players': '3-4',
+    'time': '60-120 min',
+    'description': 'A classic strategy game of resource trading and settlement building',
+    'hubs': [
+      {
+        'name': 'Central Library',
+        'available': true
+      },
+      {
+        'name': 'Marrackville Community Centre',
+        'available': false
+      },
+      {
+        'name': 'Startup Hub Coworking',
+        'available': false
+      },
+    ]
+  };
+  Map selections = {
+    'hub': '',
+    'date': '',
+  };
+
+  @override
   Widget build(BuildContext context) {
-    Map gameData = {
-      'name': 'Catan',
-      'players': '3-4',
-      'time': '60-120 min',
-      'description': 'A classic stratergy game of resource trading and settlement building',
-      'hubs': [
-        {
-          'name': 'Central Library',
-          'available': true
-        },
-        {
-          'name': 'Marrackville Community Centre',
-          'available': false
-        },
-        {
-          'name': 'Startup Hub Coworking',
-          'available': false
-        },
-      ]
-    };
-    Map selections = {
-      'hub': '',
-      'date': '',
-    };
     return Scaffold(
       appBar: mainAppBar,
       body: Padding(
@@ -40,7 +46,7 @@ class BorrowScreen extends StatelessWidget{
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               border: Border.all(
-                color: Colors.black26
+                color: Colors.black12
               ),
             ),
             child: Padding(
@@ -73,7 +79,7 @@ class BorrowScreen extends StatelessWidget{
                                 Text('60-120 min'),
                               ],
                             ),
-                            Text('A classic stratergy game of resource trading and settlement building'),
+                            Text('A classic strategy game of resource trading and settlement building'),
                             Padding(
                               padding: const EdgeInsets.symmetric(vertical: 10),
                               child: Row(
@@ -114,84 +120,14 @@ class BorrowScreen extends StatelessWidget{
                       ),
                     ],
                   ),
-                  Text(
-                    'Select Hub',
-                    style: TextStyle(
-                      fontFamily: 'worksans',
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Column(
-                    children: List.generate(
-                      gameData['hubs']?.length ?? 0,
-                      (index) {
-                        String hubName = gameData['hubs'][index]['name'] ?? 'Unknown Hub';
-                        bool hubAvailability =
-                            gameData['hubs'][index]['available'] ?? false;
-
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  hubName,
-                                  style: TextStyle(
-                                    fontFamily: 'worksans',
-                                    fontSize: 16.5,
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: (){
-                                  if(hubAvailability){
-                                    selections['hub'] = hubName;
-                                  } else{
-                                    // Implement booking logic here
-                                  }
-                                },
-                                child: Container(
-                                  width: 120,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: hubAvailability
-                                        ? MainColors.primaryColor
-                                        : Colors.transparent,
-                                    border: !hubAvailability
-                                        ? Border.all(color: MainColors.primaryColor)
-                                        : null,
-                                  ),
-                                  child: Center(
-                                    child: Padding(
-                                      padding:
-                                          const EdgeInsets.symmetric(vertical: 10),
-                                      child: Text(
-                                        hubAvailability ? 'Available' : 'Book',
-                                        style: TextStyle(
-                                          fontFamily: 'worksans',
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: hubAvailability
-                                              ? Colors.white
-                                              : MainColors.primaryColor,
-                                          decoration: hubAvailability
-                                              ? TextDecoration.underline
-                                              : null,
-                                          decorationColor: Colors.white,
-                                          decorationThickness: 2.5,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                  SelectHubSection(
+                    gameData: gameData,
+                    selections: selections,
+                    onHubSelected: (hub) {
+                      setState(() {
+                        selections['hub'] = hub;
+                      });
+                    },
                   ),
                 ],
               ),
@@ -199,6 +135,120 @@ class BorrowScreen extends StatelessWidget{
           ),
         )
       )
+    );
+  }
+}
+
+class SelectHubSection extends StatefulWidget {
+  final Map gameData;
+  final Map selections;
+  final Function(String) onHubSelected;
+
+  const SelectHubSection({
+    required this.gameData,
+    required this.selections,
+    required this.onHubSelected,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SelectHubSectionState createState() => _SelectHubSectionState();
+}
+
+class _SelectHubSectionState extends State<SelectHubSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Select Hub',
+          style: TextStyle(
+            fontFamily: 'worksans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        widget.selections['hub'] == '' ? 
+        Column(
+          children: List.generate(
+            widget.gameData['hubs']?.length ?? 0,
+            (index) {
+              String hubName = widget.gameData['hubs'][index]['name'] ?? 'Unknown Hub';
+              bool hubAvailability = widget.gameData['hubs'][index]['available'] ?? false;
+
+              return hubTile(hubName, hubAvailability: hubAvailability);
+            },
+          ),
+        ) : hubTile(widget.selections['hub'], altText: 'Back')
+      ],
+    );
+  }
+
+  Padding hubTile(String hubName, {bool hubAvailability = false, String? altText}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              hubName,
+              style: TextStyle(
+                fontFamily: 'worksans',
+                fontSize: 16.5,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              if (altText != null){
+                widget.onHubSelected('');
+              }else {
+                if (hubAvailability) {
+                  widget.onHubSelected(hubName);
+                } else {
+                  // Implement booking logic here
+                } 
+              }
+    
+            },
+            child: Container(
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: hubAvailability
+                    ? MainColors.primaryColor
+                    : Colors.transparent,
+                border: !hubAvailability
+                    ? Border.all(color: MainColors.primaryColor)
+                    : null,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    altText ?? (hubAvailability ? 'Available' : 'Book'),
+                    style: TextStyle(
+                      fontFamily: 'worksans',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: hubAvailability
+                          ? Colors.white
+                          : MainColors.primaryColor,
+                      decoration: hubAvailability
+                          ? TextDecoration.underline
+                          : null,
+                      decorationColor: Colors.white,
+                      decorationThickness: 2.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
