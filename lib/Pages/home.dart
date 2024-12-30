b0b8c9602debd0e1cbc269d9b674e5097d2d3dfc
Dylan <dmj08bot@gmail.com>
2024-12-30 17:59:22 +1100

    appbar moved to a seperate widgets file

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:board_game_borrower/Colors.dart';
+import 'package:board_game_borrower/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
@@ -7,33 +8,7 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Text(
-                  'ROLLERS GAMES HUB',
-                  style: TextStyle(
-                    fontFamily: 'Marujo',
-                  ),
-                ),
-                SizedBox(width: 10),
-                Image.asset(
-                  'Assets/logo-red.png',
-                  width: 25,
-                  height: 25,
-                )
-              ],
-            ),
-            Icon(
-              Icons.menu,
-              size: 35,
-            ),
-          ],
-        ),
-      ),
+      appBar: mainAppBar,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
@@ -59,65 +34,81 @@ class HomePage extends StatelessWidget{
               ],
             ),
             Divider(thickness: 1, color: Colors.black,),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Colors.black26
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Catan',
-                            style: TextStyle(
-                              fontFamily: 'worksans',
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.person),
-                              SizedBox(width: 4), // Space between icon and text
-                              Text('3-4'),
-                              SizedBox(width: 16), // Space between two groups
-                              Icon(Icons.access_time),
-                              SizedBox(width: 4), // Space between icon and text
-                              Text('60-120 min'),
-                            ],
-                          ),
-                          Text('A classic stratergy game of resource trading and settlement building'),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.place,
-                                color: MainColors.primaryColor,
-                                size: 30,
-                              ),
-                              Text('Central Library'),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Image.asset(
-                      'Assets/catan.png',
-                      scale: 1.2,
-                    ),
-                  ],
-                ),
-              )
-            )
+            GameTile()
           ], 
         ),
       )
+    );
+  }
+}
+
+class GameTile extends StatelessWidget {
+  const GameTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        debugPrint('Game tile tapped');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.black26
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 7.5, horizontal: 15),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Catan',
+                      style: TextStyle(
+                        fontFamily: 'worksans',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.person),
+                        SizedBox(width: 4), // Space between icon and text
+                        Text('3-4'),
+                        SizedBox(width: 16), // Space between two groups
+                        Icon(Icons.access_time),
+                        SizedBox(width: 4), // Space between icon and text
+                        Text('60-120 min'),
+                      ],
+                    ),
+                    Text('A classic stratergy game of resource trading and settlement building'),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.place,
+                          color: MainColors.primaryColor,
+                          size: 30,
+                        ),
+                        Text('Central Library'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'Assets/catan.png',
+                scale: 1.2,
+              ),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
---
+++ lib/widgets.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+AppBar mainAppBar = AppBar(
+  title: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(
+        children: [
+          Text(
+            'ROLLERS GAMES HUB',
+            style: TextStyle(
+              fontFamily: 'Marujo',
+            ),
+          ),
+          SizedBox(width: 10),
+          Image.asset(
+            'Assets/logo-red.png',
+            width: 25,
+            height: 25,
+          )
+        ],
+      ),
+      Icon(
+        Icons.menu,
+        size: 35,
+      ),
+    ],
+  ),
+);
